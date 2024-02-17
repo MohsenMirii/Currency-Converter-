4986c8d56ce5828246b85d8ffe2633585a6f95a4
Mohsen Miri <mr.mhnmri@gmail.com>
2024-02-17 15:46:00 +0100

    migrations

---
+++ Data/Migrations/20240217111500_initDB.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Currency");
+
+            migrationBuilder.CreateTable(
+                name: "ConversionRates",
+                schema: "Currency",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FromCurrency = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    ToCurrency = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Amount = table.Column<double>(type: "float", nullable: false),
+                    CreateDateTime = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    CreatorUserId = table.Column<long>(type: "bigint", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeleteDateTime = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    UpdateDateTime = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConversionRates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConversionRates",
+                schema: "Currency");
+        }
+    }
+}
